4e3be617058c0283bde1884284578e3e849c54f4
Lebed Tima <lebed.tima@gmail.com>
2014-12-18 21:34:09 +0200

    refactoring

--- sites/all/themes/praxis/templates/header.php
+++ sites/all/themes/praxis/templates/header.php
@@ -8,6 +8,25 @@
 ?>
 <div id="header-wrap">
     <div id="header">
+<!--        <div>-->
+<!--            --><?php //global $user; if ($logged_in !=true): ?>
+<!--                <div class="search">-->
+<!--                    <a href="#"><span>--><?php //print t('Search'); ?><!--</span></a>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="user_login_block">-->
+<!--                    <a href="/user" class="login-user-link"><span>--><?php //print t('Login'); ?><!--</span></a>-->
+<!--                </div>-->
+<!--            --><?php //else: ?>
+<!--                <div class="user_logout_block">-->
+<!--                    <a href="/user/logout" class="logout-link">--><?php //print t('Welcome').',';?><!-- <span>--><?php //print $user->name;  ?><!-- </span></a>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="search">-->
+<!--                    <a href="#"><span>--><?php //print t('Search'); ?><!--</span></a>-->
+<!--                </div>-->
+<!--            --><?php //endif; ?>
+<!--        </div>-->
 
         <div class="logo">
             <a href="<?php print $front_page; ?>"><h1><?php print $site_name; ?></h1></a>
@@ -15,27 +34,21 @@
 
         <div class="nav-wrap">
             <div class="nav">
-                <div class="notfall">
-                    <a href="/notfall">
-                        <span>notfall</span>
-                    </a>
-                </div>
                 <?php if ($main_menu): ?>
                     <div id="main-menu" class="navigation">
                         <?php print render($main_menu_expanded); ?>
                     </div>
                 <?php endif; ?>
+                <div class="notfall">
+                    <a href="/notfall">
+                        <span>notfall</span>
+                    </a>
+                </div>
             </div>
         </div>
 
-
-
-        <?php global $user; if ($logged_in==true): ?>
-            <a href="/user/logout" class="login-link">Logout</a>
-        <?php else: ?>
-            <a href="/user" class="login-link">Login</a>
-        <?php endif; ?>
         <?php print render($page['header']); ?>
     </div>
 </div>
+
 
--- sites/all/themes/praxis/templates/page--logger.tpl.php
+++ sites/all/themes/praxis/templates/page--logger.tpl.php
@@ -99,35 +99,7 @@ $locales = array('en' => 'en_GB', 'de' => 'de_DE');
     }(document, 'script', 'facebook-jssdk'));</script>
 <div id="page-wrapper"><div id="page">
 
-        <div id="header-wrap">
-            <div id="header">
-
-                <div class="logo">
-                    <a href="<?php print $front_page; ?>"><h1><?php print $site_name; ?></h1></a>
-                </div>
-                <?php global $user; if ($logged_in==true): ?>
-                    <a href="/user/logout" class="login-link">Logout</a>
-                <?php else: ?>
-                    <a href="/user" class="login-link">Login</a>
-                <?php endif; ?>
-                <?php print render($page['header']); ?>
-            </div>
-        </div> 
-
-        <div class="nav-wrap">
-            <div class="nav">
-                <div class="notfall">
-                    <a href="/notfall">
-                        <span>notfall</span>
-                    </a>
-                </div>
-                <?php if ($main_menu): ?>
-                    <div id="main-menu" class="navigation">
-                        <?php print render($main_menu_expanded); ?>
-                    </div> 
-                <?php endif; ?>
-            </div>
-        </div>
+        <?php include_once('header.php'); ?>
 
         <?php if ($messages): ?>
             <div id="messages"><div class="section clearfix">
